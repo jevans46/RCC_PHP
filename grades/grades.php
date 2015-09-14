1f8203aff517cc42447db8780c361d86b5c4c230
jevans46 <jevans46@student.rcc.edu>
2015-09-13 19:22:15 -0700

    Update grades.php
    Updated formatting and added comment

--- grades/grades.php
+++ grades/grades.php
@@ -1,5 +1,6 @@
 <html>
  <body>
+  <!-- uses html form data and submits to get_grade.php in order to use grade submitted by user as variable -->
  <form action="get_grade.php" method="get">
  Please enter your current grade percentage: <input type="text" name="grade"><br>
  <input type="submit">
